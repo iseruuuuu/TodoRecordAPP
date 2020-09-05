1dd73c3561020a18d031dade9cac9f8b4e647826
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-09-05 15:48:05 +0900

    Initial Commit

---
+++ voiceAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  voiceAPP
+//
+//  Created by user on 2020/09/05.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
