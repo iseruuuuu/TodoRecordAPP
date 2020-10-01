142ae41d89277fac3acefcb35d5328ea72fdd141
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-10-01 15:42:10 +0900

    listの行を２行に変える。

--- voiceAPP/recordRecord.swift
+++ voiceAPP/recordRecord.swift
@@ -1,16 +1,7 @@
-//
-//  recordRecord.swift
-//  voiceAPP
-//
-//  Created by user on 2020/09/17.
-//  Copyright © 2020 user. All rights reserved.
-//
-
-
 
 import SwiftUI
 import AVKit
-
+import AVFoundation
 
 var audios:AVAudioPlayer!
 struct recordRecord: View {
@@ -30,81 +21,87 @@ struct  Homee : View {
 // createing instance for recording.　レコードのインスタンスを作成
 @State var session : AVAudioSession!
 @State var recorder : AVAudioRecorder!
+@State var audio: AVAudioPlayer!
 @State var alert = false
 // Fetch Audios...　オーディオを取得する。
 @State var audios : [URL] = []
+    
+    /// Viewの編集モードを取得
+    @Environment(\.editMode) var envEditMode
+    
     var body: some View {
         NavigationView{
-            VStack{
+            VStack {
                 List(self.audios,id: \.self){i in
+                    VStack {
+            // printing only file name....　ファイルの印刷
+                Text(i.relativeString)
+                    Divider()
+    
+                    // Text("  \n ")
+           //共有機能
+            //ボタンを追加できた
                     
-                    // printing only file name....　ファイルの印刷
-                        Text(i.relativeString)
-             /*     Text("  \n ")
-                    Text("  \n ")
-                    Text("  \n ")
-                    Text("  \n ")
-                    Text("  \n ")
-                    Text("  \n ")
-                    Text("  \n ") */
+                HStack {
+                        
+                    
+        Button(action: {
+                print("タップさた")
+                }) {
+                Image(systemName: "ellipsis")   // システムアイコンを指定
+                    Text("")
+            
+        }
+            .foregroundColor(.blue)
+            .font(.largeTitle)
+            //15秒戻る機能
+            Button(action: {
+                print("タップされま")
+                }) {
+                Image(systemName: "gobackward.15")   // システムアイコンを指定
+                    Text("")
+            }
+           
+                    
+            //再生機能
+            Button(action: {
+                print("されました")
+                }) {
+                Image(systemName: "play.fill")   // システムアイコンを指定
+                    Text("")
+            }
+           
+
+            //15秒進める。
+            Button(action: {
+                print("タッした")
+                }) {
+                Image(systemName: "goforward.15")   // システムアイコンを指定
+                Text("")
+            }
+           
+                 
+                    
+            //捨てる動作
+            Button(action:  {
+                print("ました")
+                }) {
+                Image(systemName: "trash")   // システムアイコンを指定
+                Text("")
+            }
+            .foregroundColor(.blue)
+            
+           
+            
  
-                    
-                    
-                    //共有機能
-                    //ボタンを追加できた
-                    Button(action: {
-                        print("タップされました")
-                    }) {
-                        Image(systemName: "ellipsis")   // システムアイコンを指定
-                        Text("")
-                    }
-                    
-                   
-                    
-                    //15秒戻る機能
-                    Button(action: {
-                        print("タップされました")
-                    }) {
-                        Image(systemName: "gobackward.15")   // システムアイコンを指定
-                        Text("")
-                    }
-                    
-                    
-                    //再生機能
-                    Button(action: {
-                        print("タップされました")
-                    }) {
-                        Image(systemName: "play.fill")   // システムアイコンを指定
-                        Text("")
-                    }
-                    
-                   
-                    
-                    
-                    //15秒進める。
-                    Button(action: {
-                        print("タップされました")
-                    }) {
-                        Image(systemName: "goforward.15")   // システムアイコンを指定
-                        Text("")
-                    };
-                    
-                   
-                    //捨てる動作
-                    Button(action: {
-                        print("タップされました")
-                    }) {
-                        Image(systemName: "trash")   // システムアイコンを指定
-                        Text("")
-                    }
-                    
-                   
-                   
- 
-                    
-                    
-                    
-    }
+                }}}
+                
+                /// 編集モード時のみ削除処理を有効にする
+               //     .onDelete(perform: envEditMode?.wrappedValue.isEditing ?? false ? rowRemove : nil)
+                
+                
+          //録音のボタン
+                
             Button(action: {
     //Now going to record audio.　音声の録音
     // Intialization....初期化
@@ -170,7 +167,9 @@ struct  Homee : View {
                 .padding(.vertical, 25)
             }//タイトル
             .navigationBarTitle("録音")
+            .navigationBarItems(trailing: EditButton())
         }
+        
         .alert(isPresented: self.$alert, content: {
         
             Alert(title: Text("Error"), message: Text("Enable Acess"))
@@ -197,8 +196,14 @@ struct  Homee : View {
         }
         catch {
             print(error.localizedDescription)
+     
         }
+            
+            
     }
+        
+        
+        
 }
     //保存のリストの中身
     
@@ -211,9 +216,9 @@ struct  Homee : View {
             let result = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: .producesRelativePathURLs)
             
             
- //           self.audios.play
-  //          audios = try AVAudioPlayer(.producesRelativePathURLs)
- //           audios.play()
+ /*           self.audios.play
+           audios = try AVAudioPlayer(.producesRelativePathURLs)
+            audios.play() */
             
             //update all data from document directry....データの更新
             
@@ -239,8 +244,19 @@ struct  Homee : View {
         
     
     }
+
+    
+    func delete(at index:Int) {
+        audios.remove(at:index)
+    }
+    
+    func rowRemove(offsets: IndexSet) {
+           audios.remove(atOffsets: offsets)
+       }
     
 }
+
+
 
  
 
